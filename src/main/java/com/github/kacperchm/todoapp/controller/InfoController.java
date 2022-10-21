7aa3b69eb631883b778cbbff2f791334d1dcd176
kacperchm <chmuraka1998@gmail.com>
2022-10-21 19:37:58 +0200

    changes made between 39th and 52rd lesson

---
+++ src/main/java/com/github/kacperchm/todoapp/controller/InfoController.java
@@ -0,0 +1,29 @@
+package com.github.kacperchm.todoapp.controller;
+
+import com.github.kacperchm.todoapp.TaskConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    private DataSourceProperties dataSource;
+    private TaskConfigurationProperties myProp;
+
+    public InfoController(DataSourceProperties dataSource, TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
+
+    @GetMapping("/info/url")
+    String url() {
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    boolean myProp() {
+        return myProp.getTemplate().isAllowMultipleTasks();
+    }
+}
