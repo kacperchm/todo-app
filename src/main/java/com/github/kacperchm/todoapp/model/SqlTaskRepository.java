108600b1dd1b619e6f5478f236aaafd609dc4c59
kacperchm <chmuraka1998@gmail.com>
2022-10-22 12:50:14 +0200

    changes made between 53rd and 66th lesson

--- src/main/java/com/github/kacperchm/todoapp/model/SqlTaskRepository.java
+++ src/main/java/com/github/kacperchm/todoapp/model/SqlTaskRepository.java
@@ -1,6 +1,7 @@
 package com.github.kacperchm.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import java.util.List;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
-    
+    @Override
+    @Query(nativeQuery = true, value = "SELECT COUNT(*)>0 FROM tasks WHERE id=:id")
+    boolean existsById(@Param("id") Integer id);
 }
